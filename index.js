45106eec2f28c29073175fbc6b849f3ca0fee574
Ashley <dillia23@uw.edu>
2016-05-23 22:06:38 -0700

    change bars from divs to svgs

--- index.js
+++ index.js
@@ -26,15 +26,30 @@ function incomingData() {
 }
 
 function createBarChart(data) {
+	var width = 1500,
+    barHeight = 20;
 	var x = d3.scale.linear()
-        .domain([0, d3.max(data, function(d) { return d.aawdt })])
-        .range([0, 420]);
-	var chart = d3.select(".chart");
-	chart.selectAll("div")
-		.data(data)
-		.enter().append("div").attr("class", "bar")
-		.style("width", function(d) { return x(d.aawdt) + "px"; })
-		.text(function(d) { return d.stname; });
+        .domain([0, d3.max(data, function(d) { return parseInt(d.aawdt) })])
+        .range([0, width]);
+    console.log(x(data[0].aawdt));
+	var chart = d3.select(".chart")
+		.attr("width", width)
+    	.attr("height", barHeight * data.length);
+
+	var bar = chart.selectAll("g")
+	    .data(data)
+	    .enter().append("g")
+	    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+	bar.append("rect")
+	    .attr("width", function(d) { return x(d.aawdt); })
+	    .attr("height", barHeight - 1);
+
+	bar.append("text")
+	    .attr("x", function(d) { return x(d.aawdt) - 3; })
+	    .attr("y", barHeight / 2)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.stname; });
 }
 
 //create initial set of bar charts
